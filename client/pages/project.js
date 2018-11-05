6525d92f5bd5301146756a32adbabf3c2198973f
Leonard Martin <leonard.martin@gmail.com>
2018-11-05 12:16:29 +0000

    Add sections subheading

--- client/pages/project.js
+++ client/pages/project.js
@@ -24,6 +24,7 @@ class Index extends React.Component {
   render() {
     return <React.Fragment>
       <h1>{ this.props.title }</h1>
+      <h2>Sections</h2>
       <ApplicationSummary project={ this.props.id } />
       <p className="control-panel"><Link to="/">&lt; Back to project list</Link></p>
     </React.Fragment>
