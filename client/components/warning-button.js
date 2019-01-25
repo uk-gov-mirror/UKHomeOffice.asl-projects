781dc282954f7bc28fa070ee2a9c8ea726bc7947
Leonard Martin <leonard.martin@gmail.com>
2019-01-25 10:05:11 +0000

    Add a warning button component
    Makes a person click a button twice before doing the really destructive thing

---
+++ client/components/warning-button.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Button } from '@ukhomeoffice/react-components';
+
+class WarningButton extends React.Component {
+
+  warn (e) {
+    e.preventDefault();
+    this.setState({ warning: true });
+  }
+
+  render() {
+    if (this.state && this.state.warning) {
+      return <Button { ...this.props }>{this.props.warning}</Button>
+    }
+    return <Button { ...this.props } onClick={e => this.warn(e)} />;
+  }
+
+}
+
+WarningButton.defaultProps = {
+  warning: 'Are you sure?'
+}
+
+export default WarningButton;
