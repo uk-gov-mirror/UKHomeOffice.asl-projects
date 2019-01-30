8ea72bd7fb898e33c50431b34aabe915fb4950cb
Leonard Martin <leonard.martin@gmail.com>
2019-01-30 11:43:28 +0000

    Allow unused const for polyfill import

--- client/index.js
+++ client/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+// eslint-disable-next-line no-unused-vars
 import DetailsPolyfill from 'details-element-polyfill';
 
 import Alert from './components/alert';
