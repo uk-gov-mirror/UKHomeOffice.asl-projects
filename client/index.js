197fc958d60756db0786d830851499cb51fe5605
Leonard Martin <leonard.martin@gmail.com>
2019-02-07 11:34:58 +0000

    Add polyfills for Element.prototype.closest

--- client/index.js
+++ client/index.js
@@ -1,8 +1,8 @@
+import './polyfills';
+
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-// eslint-disable-next-line no-unused-vars
-import DetailsPolyfill from 'details-element-polyfill';
 
 import Alert from './components/alert';
 import Router from './router';
