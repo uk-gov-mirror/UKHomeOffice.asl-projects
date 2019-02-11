5dffce7b7b75f5bf8fd3cc11eebe9dc701d86d45
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2019-02-11 16:07:52 +0000

    provide links to the relevant sections from nts

--- client/pages/sections/nts.js
+++ client/pages/sections/nts.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { connectProject } from '../../helpers';
 import classnames from 'classnames';
 
@@ -104,13 +105,16 @@ class NTSSummary extends Component {
                   <h2 ref={this.sectionRefs[section]}>{title}</h2>
                   {
                     this.getFields(this.props.sectionsSettings[section], fields).map(field => (
-                      <Review
-                        {...field}
-                        label={ field.review || field.label }
-                        key={ field.name }
-                        value={ this.props.project && this.props.project[field.name] }
-                        onEdit={ () => console.log('edit') }
-                      />
+                    <Review
+                      {...field}
+                      label={ field.review || field.label }
+                      key={ field.name }
+                      value={ this.props.project && this.props.project[field.name] }
+                      editLink={() => {
+                        const actualSection = section === 'introduction' ? 'introduction/1' : section;
+                        return <Link to={`/project/${this.props.project.id}/${actualSection}#${field.name}`}>Edit</Link>;
+                      }}
+                    />
                     ))
                   }
                   <hr />
