25d08eab8b71ef28e5b4cb3eb9fdeb87820543a2
Joe Fitter <joefitter@hotmail.com>
2019-02-22 14:45:16 +0000

    fix lint

--- client/components/static-section.js
+++ client/components/static-section.js
@@ -3,16 +3,15 @@ import { connect } from 'react-redux';
 import flatten from 'lodash/flatten';
 import ReviewFields from './review-fields';
 
-const StaticSection = ({ section, project, fields = [] }) => (
-  <Fragment>
-    <h1>{section.title}</h1>
-    {
-      section.review
-        ? <section.review {...section} fields={fields} values={project} readonly={true} />
-        : <ReviewFields fields={fields} values={project} readonly={true} />
-    }
-  </Fragment>
-)
+const StaticSection = ({ section, project, fields = [] }) => {
+  const Component = section.review || ReviewFields;
+  return (
+    <Fragment>
+      <h1>{section.title}</h1>
+      <Component {...section} fields={fields} values={project} readonly={true} />
+    </Fragment>
+  )
+}
 
 const mapStateToProps = ({ project }, { section }) => {
   const fields = flatten([
