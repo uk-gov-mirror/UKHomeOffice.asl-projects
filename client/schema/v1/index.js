5d2f48b524bc7776f2739543ead4d29643a45f47
Joe Fitter <joefitter@hotmail.com>
2019-03-26 16:16:26 +0000

    updated values to preserve data

--- client/schema/v1/index.js
+++ client/schema/v1/index.js
@@ -65,51 +65,51 @@ export default {
                 options: [
                   {
                     label: '(a) Basic research',
-                    value: 'purpose-a'
+                    value: 'basic-research'
                   },
                   {
                     label: '(b) Translational or applied research with one of the following aims:',
-                    value: 'purpose-b',
+                    value: 'translational-research',
                     reveal: {
-                      name: 'purpose-b',
+                      name: 'translational-research',
                       label: '',
                       type: 'checkbox',
                       className: 'smaller',
                       options: [
                         {
                           label: '(i) Avoidance, prevention, diagnosis or treatment of disease, ill-health  or abnormality, or their effects, in man, animals or plants.',
-                          value: 'purpose-b1',
+                          value: 'translational-research-1',
                         },
                         {
                           label: '(ii) Assessment, detection, regulation or modification of physiological conditions in man, animals or plants.',
-                          value: 'purpose-b2'
+                          value: 'translational-research-2'
                         },
                         {
                           label: '(iii) Improvement of the welfare of animals or of the production conditions for animals reared for agricultural purposes.',
-                          value: 'purpose-b3'
+                          value: 'translational-research-3'
                         }
                       ]
                     }
                   },
                   {
                     label: '(c) Development, manufacture or testing of the quality, effectiveness and safety of drugs, foodstuffs and feedstuffs or any other substances or products, with one of the following aims mentioned in paragraph (b)',
-                    value: 'purpose-c'
+                    value: 'safety-of-drugs'
                   },
                   {
                     label: '(d) Protection of the natural environment in the interests of the health or welfare of man or animals.',
-                    value: 'purpose-d'
+                    value: 'protection-of-environment'
                   },
                   {
                     label: '(e) Research aimed at preserving the species of animal subjected to regulated procedures as part of the programme of work.',
-                    value: 'purpose-e'
+                    value: 'preservation-of-species'
                   },
                   {
                     label: '(f) Higher education or training for the acquisition, maintenance or improvement of vocational skills.',
-                    value: 'purpose-f'
+                    value: 'higher-education'
                   },
                   {
                     label: '(g) Forensic enquiries.',
-                    value: 'purpose-g'
+                    value: 'forensic-enquiries'
                   }
                 ]
               },
