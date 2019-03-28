de9c56ec01493f2d48bae603c5e4a3483b31dbdc
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2019-03-28 10:37:29 +0000

    check readonly

--- client/components/completable.js
+++ client/components/completable.js
@@ -2,17 +2,23 @@ import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import classnames from 'classnames';
 
-const Completable = ({ status = 'incomplete', legacy, children }) => (
-  <Fragment>
-    { !legacy &&
-      <label className={classnames('status-label', status)}>
-        { status === 'incomplete' ? 'In progress' : 'Completed' }
-      </label>
-    }
-    { children }
-  </Fragment>
-)
+const Completable = ({ status = 'incomplete', legacy, readonly, children }) => {
+  if (legacy || readonly) {
+    return children;
+  }
 
-const mapStateToProps = ({ application: { schemaVersion } }) => ({ legacy: schemaVersion === 0 });
+  return (
+    <Fragment>
+      {
+        <label className={classnames('status-label', status)}>
+          { status === 'incomplete' ? 'In progress' : 'Completed' }
+        </label>
+      }
+      { children }
+    </Fragment>
+  );
+};
+
+const mapStateToProps = ({ application: { schemaVersion, readonly } }) => ({ legacy: schemaVersion === 0, readonly });
 
 export default connect(mapStateToProps)(Completable);
