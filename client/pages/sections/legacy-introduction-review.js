5f30fc0598d80bb3a4843dc354f266341e37144f
Joe Fitter <joefitter@hotmail.com>
2019-07-30 08:39:59 +0100

    fix lint

--- client/pages/sections/legacy-introduction-review.js
+++ client/pages/sections/legacy-introduction-review.js
@@ -3,19 +3,19 @@ import classnames from 'classnames';
 import { connect } from 'react-redux';
 import format from 'date-fns/format';
 import ReviewFields from '../../components/review-fields';
+import Banner from '../../components/banner';
 import { DATE_FORMAT } from '../../constants';
 
-const LegacyIntroduction = ({ fields, project, values, pdf, readonly }) => (
+const LegacyIntroduction = ({ fields, project, values, pdf, readonly, title }) => (
   <div className={classnames('introduction-review', { readonly })}>
     {
       !readonly && (
         <Fragment>
           <Banner>
             <h2>Please review your answers for</h2>
-            <h1>Action plan</h1>
+            <h1>{ title }</h1>
           </Banner>
-          <h1>Action plan</h1>
-          <Playback playback={playback} />
+          <h1>{ title }</h1>
         </Fragment>
       )
     }
@@ -26,14 +26,22 @@ const LegacyIntroduction = ({ fields, project, values, pdf, readonly }) => (
     {
       readonly && !pdf && (
         <Fragment>
-          <div className="granted-section">
-            <h3>Date granted</h3>
-            <p>{format(project.issueDate, DATE_FORMAT.long)}</p>
-          </div>
-          <div className="granted-section">
-            <h3>Expiry date</h3>
-            <p>{format(project.expiryDate, DATE_FORMAT.long)}</p>
-          </div>
+          {
+            project.issueDate && (
+              <div className="granted-section">
+                <h3>Date granted</h3>
+                <p>{format(project.issueDate, DATE_FORMAT.long)}</p>
+              </div>
+            )
+          }
+          {
+            project.expiryDate && (
+              <div className="granted-section">
+                <h3>Expiry date</h3>
+                <p>{format(project.expiryDate, DATE_FORMAT.long)}</p>
+              </div>
+            )
+          }
         </Fragment>
       )
     }
