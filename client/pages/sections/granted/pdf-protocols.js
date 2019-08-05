7ade2fdf45efe929561e9c5b61af265c4314beb5
Leonard Martin <leonard.martin@gmail.com>
2019-08-05 13:33:23 +0100

    Fix potentially missing label on null species
    Some production protocols have null species, which makes things crash when we expect them to always exist.

--- client/pages/sections/granted/pdf-protocols.js
+++ client/pages/sections/granted/pdf-protocols.js
@@ -10,9 +10,10 @@ const Protocol = ({ protocol, number, sections, isLegacy }) => {
   const species = !isLegacy
     ? (protocol.speciesDetails || []).filter(s => s.name)
     : (protocol.species || []).map(s => {
+      const matched = LEGACY_SPECIES.find(ls => ls.value === s.speciesId);
       return {
         ...s,
-        name: LEGACY_SPECIES.find(ls => ls.value === s.speciesId).label
+        name: matched ? matched.label : '-'
       }
     });
   return (
