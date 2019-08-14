58abdcb3f0bba5f85867693f2f2245db23b2576f
Elvin Ali <ali.elvin@gmail.com>
2019-08-14 11:11:16 +0100

    USe const instead of let

--- client/components/application-summary.js
+++ client/components/application-summary.js
@@ -149,7 +149,7 @@ class ApplicationSummary extends React.Component {
                 {
                   subsections.map(key => {
                     const subsection = section.subsections[key];
-                    let fields = Object.values(this.props.fieldsBySection[key] || []);
+                    const fields = Object.values(this.props.fieldsBySection[key] || []);
                     if(subsection.repeats) {
                       fields.push(subsection.repeats);
                     }
