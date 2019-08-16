1a015134f2b2c20954a4daeb70295e8304afa14a
Joe Fitter <joefitter@hotmail.com>
2019-08-16 13:32:11 +0100

    Reload on session expiry
    * disable rediect in fetch
    * if detect redirect attempted, reload page

--- client/actions/messaging.js
+++ client/actions/messaging.js
@@ -4,11 +4,19 @@ export default ({ method, data, url }) => {
   const params = {
     method,
     credentials: 'include',
-    json: data
+    // prevent auto-redirecting request
+    // to keycloak if users session expires
+    redirect: 'manual',
+    json: data,
   };
   return Promise.resolve()
     .then(() => fetch(url, params).response)
     .then(response => {
+      // detect if redirect was attempted
+      if (response.type === 'opaqueredirect') {
+        window.onbeforeunload = null;
+        window.location.reload();
+      }
       return response.json()
         .then(json => {
           if (response.status > 399) {
