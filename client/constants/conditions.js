2bdaf05635f26a51fe34fafd6fbbc92128705d99
pcuk1979 <philip.cox@digital.homeoffice.gov.uk>
2019-08-19 14:05:10 +0100

    Text changes
    As per https://jira.digital.homeoffice.gov.uk/browse/AS-1912

--- client/constants/conditions.js
+++ client/constants/conditions.js
@@ -29,7 +29,7 @@ export default {
       versions: [
         {
           title: 'Re-use',
-          content: '<<<INSERT animal type(s) HERE>>> may be re-used in this protocol where re-use is proposed from this protocol, from another protocol or from another project, provided that all criteria in section 14 of the Animals (Scientific Procedures) Act and in this project licence are fulfilled.'
+          content: '<<<INSERT animal type(s) HERE>>> that have been kept alive and maintained under the supervision  of the NVS at <<<INSERT place HERE.>>> may be re-used in this protocol, provided that all criteria in section 14 of the Animals (Scientific Procedures) Act and in this project licence are fulfilled.'
         }
       ]
     },
