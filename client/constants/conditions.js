ce2f71f10514a719171bb9a8aa2b3934dce436c7
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2019-10-14 14:02:26 +0100

    remove existing anaesthesia conditions

--- client/constants/conditions.js
+++ client/constants/conditions.js
@@ -267,53 +267,6 @@ For studies requested for other worldwide authorities, for example the US Food a
         }
       ]
     },
-    anaesthesia: {
-      checked: true,
-      type: 'condition',
-      versions: [
-        {
-          title: 'Anaesthesia',
-          content: `Induction and maintenance of general or local anaesthesia, sedation or analgesia to mitigate the pain, suffering or distress associated with the performance of other regulated procedures is indicated using the following codes in protocols:
-* AA no anaesthesia
-* AB general anaesthesia with recovery
-* AC non-recovery general anaesthesia
-* AD under neuromuscular blockade`
-        }
-      ]
-    },
-    'general-anaesthesia': {
-      type: 'condition',
-      versions: [
-        {
-          title: 'General anaesthesia',
-          content: 'Unless otherwise specified, all animals are expected to make a rapid and unremarkable recovery from the anaesthetic within two hours. Uncommonly animals that fail to do so or exhibit signs of pain, distress or of significant ill health should be killed unless a programme of enhanced monitoring and care is instituted until the animal fully recovers.'
-        }
-      ]
-    },
-    surgery: {
-      type: 'condition',
-      versions: [
-        {
-          title: 'Surgery',
-          content: `Unless otherwise specified:
-
-* Surgical procedures should be carried out aseptically, to at least the published Home Office minimum;
-* In the uncommon event of post-operative complications, animals will be killed unless, in the opinion of a veterinary surgeon, such complications can be remedied promptly and successfully using no more than minor interventions. Minimally inflamed wounds without obvious infection may be re-closed on one occasion within 48 hours of the initial surgery. In the event of recurrence, NVS advice will be followed;
-* Peri and post-operative analgesia will be provided; agents will be administered as agreed in advance with the NVS;
-* All animals are expected to make a rapid and unremarkable recovery from the anaesthetic within two hours. Uncommonly animals that fail to do so or exhibit signs of pain, distress or of significant ill health will be killed by a Schedule 1 method unless a programme of enhanced monitoring and care is instituted until the animal fully recovers;
-* Any animal not fully recovered from the surgical procedure within 24 hrs (eating, drinking and return to normal behaviour) should be killed.`
-        }
-      ]
-    },
-    'admin-of-substances': {
-      type: 'condition',
-      versions: [
-        {
-          title: 'Administration of substances and withdrawal of fluids',
-          content: 'Unless otherwise specified, administration of substances and withdrawal of body fluids will be undertaken using a combination of volumes, routes, and frequencies that of themselves will result in no more than transient discomfort and no lasting harm using published guidelines on minimal severity.'
-        }
-      ]
-    },
     'batch-testing': {
       type: 'condition',
       versions: [
