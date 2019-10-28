5047fae2421ae10d11173a80932fc6870c299b77
Joe Fitter <joefitter@hotmail.com>
2019-10-28 15:01:02 +0000

    Feature - clear formatting
    * added button to toolbar
    * clear marks in selection
    * find ancestor element, find all child nodes and unwrap up to ancestor
    * treat table cells like new document

--- client/components/editor/blocks/index.js
+++ client/components/editor/blocks/index.js
@@ -1,22 +1,64 @@
 import renderBlock from './render-block';
 
-const DEFAULT_NODE = 'paragraph';
+const DEFAULT_BLOCK = 'paragraph';
 
 const hasBlock = (editor, event, type) => {
   const { value } = editor;
   return value.blocks.some(node => node.type == type);
 };
 
+const findRootElem = (document, block) => {
+  const parent = document.getParent(block.key);
+  if (parent.object === 'document' || parent.type === 'table-cell') {
+    return block;
+  }
+  return findRootElem(document, parent);
+}
+
+const unwrapBlock = (editor, block) => {
+  const { value } = editor;
+  const { document } = value;
+  block = block || value.startBlock;
+
+  const parent = document.getParent(block.key);
+
+  if (parent.object === 'document' || parent.type === 'table-cell') {
+    return null;
+  }
+
+  editor.withoutNormalizing(() => {
+    editor.unwrapBlockByKey(parent.key);
+    unwrapBlock(editor, block);
+  });
+
+}
+
+const unwrapBlocks = editor => {
+  const { value } = editor;
+  const { document } = value;
+
+  if (!value.selection.start.key) {
+    return null;
+  }
+
+  const root = findRootElem(document, value.startBlock);
+  const childBlocks = root.getBlocks();
+
+  childBlocks.forEach(block => unwrapBlock(editor, block));
+  editor.setBlocks(DEFAULT_BLOCK);
+}
+
 const toggleBlock = (editor, type) => {
   const isActive = hasBlock(editor, type);
-  editor.setBlocks(isActive ? DEFAULT_NODE : type);
+  editor.setBlocks(isActive ? DEFAULT_BLOCK : type);
 };
 
 export default function Blocks() {
   return {
     renderBlock,
     commands: {
-      toggleBlock
+      toggleBlock,
+      unwrapBlocks
     },
     queries: {
       hasBlock
