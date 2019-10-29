8e6d9c4515ea1a4a5c9274b881cb363df49b75a7
Joe Fitter <joefitter@hotmail.com>
2019-10-29 08:45:43 +0000

    Bugfix - step title comments
    Replaced text editor component with ReviewFields to handle read only view with comments

--- client/pages/sections/protocols/steps.js
+++ client/pages/sections/protocols/steps.js
@@ -1,12 +1,11 @@
 import React, { Component, Fragment, createRef } from 'react';
-import { withRouter } from 'react-router';
+import { useParams } from 'react-router';
 
 import classnames from 'classnames'
 import { Button } from '@ukhomeoffice/react-components';
 
 import every from 'lodash/every';
 import isUndefined from 'lodash/isUndefined';
-import TextEditor from '../../../components/editor';
 
 import ReviewFields from '../../../components/review-fields';
 import Repeater from '../../../components/repeater';
@@ -81,7 +80,7 @@ class Step extends Component {
   }
 
   render() {
-    const { prefix, index, fields, values, updateItem, length, editable, deleted } = this.props;
+    const { prefix, index, fields, values, updateItem, length, editable, deleted, step } = this.props;
 
     const completed = !editable || values.completed;
     return (
@@ -112,7 +111,15 @@ class Step extends Component {
             }
           </h3>
           {
-            completed && values.title && <TextEditor value={values.title} readOnly={true} key={values.title} />
+            completed && values.title && (
+              <ReviewFields
+                fields={[ fields.find(f => f.name === 'title') ]}
+                values={{ title: values.title }}
+                prefix={this.props.prefix}
+                editLink={`0#${this.props.prefix}`}
+                readonly={step !== 1}
+              />
+            )
           }
         </Fragment>
         {
@@ -135,6 +142,7 @@ class Step extends Component {
               values={values}
               prefix={this.props.prefix}
               editLink={`0#${this.props.prefix}`}
+              readonly={step !== 1}
             />
             {
               editable && !deleted && <a href="#" onClick={this.editStep}>Edit step</a>
@@ -146,7 +154,8 @@ class Step extends Component {
   }
 }
 
-const Steps = ({ values, prefix, updateItem, editable, ...props }) => {
+export default function Steps({ values, prefix, updateItem, editable, ...props }) {
+  const { step } = useParams();
   return (
     <div className="steps">
       <p className="grey">{props.hint}</p>
@@ -163,11 +172,10 @@ const Steps = ({ values, prefix, updateItem, editable, ...props }) => {
         <Step
           editable={editable}
           deleted={values.deleted}
+          step={step}
           { ...props }
         />
       </Repeater>
     </div>
   )
 }
-
-export default withRouter(Steps);
