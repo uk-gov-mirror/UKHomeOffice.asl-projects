a45950e1bd105deee89e4b09df7d4a23c68a9e94
Joe Fitter <joefitter@hotmail.com>
2019-12-16 11:12:37 +0000

    Update to legal text
    * updated text, added alt label for reviewing duration - not a question.
    * If review property on field, render this rather than the label

--- client/constants/legal.js
+++ client/constants/legal.js
@@ -1,13 +1,13 @@
 module.exports = {
-  licenceHolder: `This **PROJECT LICENCE** allows the holder to carry out a programme of scientific procedures on living animals under the powers vested in the Secretary of State by the **ANIMALS (SCIENTIFIC PROCEDURES) ACT 1986.**
+  licenceHolder: `This **PROJECT LICENCE** permits the licence holder to carry out a programme of scientific procedures on living animals under the **ANIMALS (SCIENTIFIC PROCEDURES) ACT 1986**.
 
-  The Secretary of State hereby licenses the project licence holder to carry out the programme of work specified, subject to the restrictions and provisions contained in the Act and subject also to the limitations and conditions contained in this licence and to any other conditions that the Secretary of State may from time to time prescribe.
+The project licence holder may carry out the specified programme of work, subject to the restrictions and provisions contained within the Act and any limitations and conditions specified within this licence or by the Secretary of State.
 
-This licence does not authorise the project licence holder or any other person to carry out procedures on any animals unless they hold a personal licence issued under the Act which authorises them to carry out those procedures on the animals of those types`,
-  grantedAuthority: licenceNumber => `This licence has been granted on the basis of information provided in an application for this licence.
-
-Under this project licence numbered ${licenceNumber} the Secretary of State grants authority only for:
-* work to meet the aim of the project specified;
-* the procedures and types of animal types specified; and
-* work at the place or places specified in the licence`
+This licence does not authorise the holder or any other person to carry out procedures on any animals unless they hold an appropriate personal licence issued under the Act.`,
+  grantedAuthority: `This licence has been granted based on the information provided during the application process.
+  
+This licence authorises, only:
+* work to meet the specified project aims
+* use of specified animals and procedures
+* work at the specified places`
 };
