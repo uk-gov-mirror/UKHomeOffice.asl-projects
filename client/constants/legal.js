b26e9595bbf0c9e097e44e9aa974a21bfd578bee
Joe Fitter <joefitter@hotmail.com>
2019-12-16 11:25:23 +0000

    fix irregular whitesoace

--- client/constants/legal.js
+++ client/constants/legal.js
@@ -1,11 +1,11 @@
 module.exports = {
-  licenceHolder: `This **PROJECT LICENCE** permits the licence holder to carry out a programme of scientific procedures on living animals under the **ANIMALS (SCIENTIFIC PROCEDURES) ACT 1986**.
+  licenceHolder: `This **PROJECT LICENCE** permits the licence holder to carry out a programme of scientific procedures on living animals under the **ANIMALS (SCIENTIFIC PROCEDURES) ACT 1986**.
 
 The project licence holder may carry out the specified programme of work, subject to the restrictions and provisions contained within the Act and any limitations and conditions specified within this licence or by the Secretary of State.
 
 This licence does not authorise the holder or any other person to carry out procedures on any animals unless they hold an appropriate personal licence issued under the Act.`,
   grantedAuthority: `This licence has been granted based on the information provided during the application process.
-  
+
 This licence authorises, only:
 * work to meet the specified project aims
 * use of specified animals and procedures
