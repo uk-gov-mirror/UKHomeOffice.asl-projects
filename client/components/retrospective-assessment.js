c97aece68d8e9c593dce98f61044466ae611fde1
Leonard Martin <leonard.martin@gmail.com>
2020-02-28 16:19:00 +0000

    Use value returned on API data to determine if RA is required
    The RA required flag is set on the API so it can be used in project ladning pages. No need to keep a separate calculation here.

--- client/components/retrospective-assessment.js
+++ client/components/retrospective-assessment.js
@@ -1,28 +1,10 @@
 import React, { Fragment, useState } from 'react';
 import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import { updateRetrospectiveAssessment } from '../actions/projects';
-import isUndefined from 'lodash/isUndefined';
-import isPlainObject from 'lodash/isPlainObject';
 import { Button } from '@ukhomeoffice/react-components';
 import Field from './field';
 import RAContent from '../constants/retrospective-assessment';
 import raApplies from '../helpers/retrospective-assessment';
-
-function getInitialState(project) {
-  if (!isUndefined(project.retrospectiveAssessment)) {
-    // legacy licences may have an object containing a boolean/
-    if (isPlainObject(project.retrospectiveAssessment)) {
-      return project.retrospectiveAssessment['retrospective-assessment-required'];
-    }
-    // now saved as a boolean
-    return !!project.retrospectiveAssessment;
-  }
-  // previous new licences contained a 'retrospective-assessment' condition.
-  if (project.conditions && project.conditions.find(c => c.key.match(/^retrospective-assessment$/))) {
-    return true;
-  }
-  return false;
-}
 
 const selector = ({ project, application: { editConditions } }) => ({ project, editConditions });
 
@@ -46,7 +28,7 @@ export default function RetrospectiveAssessment({ showTitle = true }) {
   }
 
   const [isChanging, setIsChanging] = useState(false);
-  const [raRequired, setRaRequired] = useState(getInitialState(project));
+  const [raRequired, setRaRequired] = useState(project.retrospectiveAssessment);
   const dispatch = useDispatch();
 
   function cancel() {
