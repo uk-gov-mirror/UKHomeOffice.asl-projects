2303c01756b621751efebcdfbd606d47965c9924
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2020-03-09 16:26:10 +0000

    prevent missing protocols throwing an error

--- client/components/reordered-badge.js
+++ client/components/reordered-badge.js
@@ -20,7 +20,7 @@ const selector = ({
 export default function ReorderedBadge({ id }) {
   const { protocols, previous, isGranted } = useSelector(selector, shallowEqual);
   const previousIds = previous.filter(p => !(protocols.find(protocol => protocol.id === p) || {}).deleted);
-  const ids = protocols.filter(p => !p.deleted).filter(p => previous.includes(p.id)).map(p => p.id);
+  const ids = (protocols || []).filter(p => !p.deleted).filter(p => previous.includes(p.id)).map(p => p.id);
 
   const previousIndex = previousIds.indexOf(id);
   const newIndex = ids.indexOf(id);
