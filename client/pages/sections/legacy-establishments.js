d237af2b99d93f5b28f0c1b3a4de382a6fc91415
Joe Fitter <joefitter@hotmail.com>
2020-03-17 11:05:05 +0000

    include field for legacy licences, hide section on pdf

--- client/pages/sections/legacy-establishments.js
+++ client/pages/sections/legacy-establishments.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import RepeaterReview from './repeater/review';
 
-const LegacyEstablishments = ({ establishment, ...props }) => {
+const LegacyEstablishments = ({ establishment, pdf, ...props }) => {
 
   const {
     name,
@@ -11,15 +11,20 @@ const LegacyEstablishments = ({ establishment, ...props }) => {
 
   return (
     <Fragment>
-      <div className="granted-section">
-        <h2>Primary establishment</h2>
-        <dl className="inline">
-          <dt>Establishment name: </dt>
-          <dd>{ name }</dd>
-          <dt>Licence number: </dt>
-          <dd>{ licenceNumber }</dd>
-        </dl>
-      </div>
+      {
+        pdf && (
+          <div className="granted-section">
+            <h2>Primary establishment</h2>
+            <dl className="inline">
+              <dt>Establishment name: </dt>
+              <dd>{ name }</dd>
+              <dt>Licence number: </dt>
+              <dd>{ licenceNumber }</dd>
+            </dl>
+          </div>
+        )
+      }
+
       <RepeaterReview {...props} />
     </Fragment>
   )
