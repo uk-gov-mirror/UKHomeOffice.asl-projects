5afa38bd8ba1a39f00875881b2c22c1908244991
Joe Fitter <joefitter@hotmail.com>
2020-08-03 16:57:08 +0100

    added training section, bumped components with training table

---
+++ client/pages/sections/training.js
@@ -0,0 +1,58 @@
+import React, { Fragment, useRef } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useSelector, shallowEqual } from 'react-redux';
+import { TrainingSummary } from '@asl/components';
+import { Button } from '@ukhomeoffice/react-components';
+import Fieldset from '../../components/fieldset';
+import ReviewFields from '../../components/review-fields';
+
+export default function Training(props) {
+  const { training, basename, readonly } = useSelector(state => state.application, shallowEqual);
+  const project = useSelector(state => state.project);
+  const form = useRef(null);
+  const history = useHistory();
+  const needsUpdate = project['update-training'];
+
+  function onSubmit(e) {
+    e.preventDefault();
+    if (!needsUpdate) {
+      props.save({
+        training,
+        'training-complete': true
+      });
+      return history.push('/');
+    }
+    props.save('training-complete', false);
+    form.current.submit();
+  }
+
+  return (
+    <Fragment>
+      <h1>Training</h1>
+      <p>{props.intro}</p>
+      <h2>Training record</h2>
+      <TrainingSummary certificates={readonly ? project.training : training} />
+      {
+        readonly
+          ? <ReviewFields {...props} />
+          : (
+            <form
+              ref={form}
+              action={`${basename}/update-training`}
+              onSubmit={onSubmit}
+              method="POST"
+            >
+
+              <Fieldset
+                {...props}
+                values={{ 'training-update': needsUpdate }}
+                onFieldChange={props.save}
+              />
+              <Button>Continue</Button>
+            </form>
+          )
+      }
+
+    </Fragment>
+  )
+}
