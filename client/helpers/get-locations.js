607a6ec9c1fb32ec521feec038492260f7681f9b
Joe Fitter <joefitter@hotmail.com>
2020-11-13 13:41:06 +0100

    save name of transfer to est and play back in location selector if a transfer

--- client/helpers/get-locations.js
+++ client/helpers/get-locations.js
@@ -2,7 +2,7 @@ export default function getLocations (project, establishment) {
   const establishments = (project.establishments || []).filter(e => e.name || e['establishment-name']).map(e => e.name || e['establishment-name']);
   const poles = (project.polesList || []).filter(p => p.title).map(p => p.title);
   return [
-    establishment.name,
+    project.transferToEstablishmentName || establishment.name,
     ...establishments,
     ...poles
   ];
