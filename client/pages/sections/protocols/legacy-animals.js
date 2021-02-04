a9e75f3aa1d92b96947416574a72261a5a2573a8
Joe Fitter <joefitter@hotmail.com>
2021-02-04 13:38:31 +0100

    Bugfix - remove species
    * Species details cannot be removed from legacy protocols

--- client/pages/sections/protocols/legacy-animals.js
+++ client/pages/sections/protocols/legacy-animals.js
@@ -4,28 +4,44 @@ import Fieldset from '../../../components/fieldset';
 import Repeater from '../../../components/repeater';
 import ReviewFields from '../../../components/review-fields';
 
-const LegacyAnimal = ({ updateItem, values, readonly, prefix, fields, index, editable }) => (
-  <Fragment>
-    {
-      (!readonly && editable)
-        ? <Fieldset
-          prefix={prefix}
-          fields={fields}
-          values={values}
-          index={index}
-          type="species"
-          onFieldChange={(key, value) => updateItem({ [key]: value })}
-        />
-        : <ReviewFields
-          prefix={prefix}
-          fields={fields}
-          values={values}
-          editLink={`0#${prefix}`}
-        />
+function LegacyAnimal({ updateItem, values, readonly, prefix, fields, index, editable, removeItem, length }) {
+  function removeSpecies(e) {
+    e.preventDefault();
+    if (window.confirm('Are you sure you want to remove this animal type?')) {
+      removeItem();
     }
-    <hr />
-  </Fragment>
-);
+  }
+
+  return (
+    <Fragment>
+      {
+        (!readonly && editable)
+          ? (
+            <Fragment>
+              <Fieldset
+                prefix={prefix}
+                fields={fields}
+                values={values}
+                index={index}
+                type="species"
+                onFieldChange={(key, value) => updateItem({ [key]: value })}
+              />
+              {
+                length > 1 && <p><a href="#" onClick={removeSpecies}>Remove</a></p>
+              }
+            </Fragment>
+          )
+          : <ReviewFields
+            prefix={prefix}
+            fields={fields}
+            values={values}
+            editLink={`0#${prefix}`}
+          />
+      }
+      <hr />
+    </Fragment>
+  );
+}
 
 const LegacyAnimals = ({ updateItem, title, values, fields, prefix, readonly, index, editable }) => (
   <Fragment>
