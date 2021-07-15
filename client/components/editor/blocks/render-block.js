247f403088342592bbffcdb8a4ca37b9335c0329
Joe Fitter <joefitter@hotmail.com>
2021-07-15 11:23:15 +0200

    Bugfix - restore images
    * images are blocks, they do not have text, we still want to render them

--- client/components/editor/blocks/render-block.js
+++ client/components/editor/blocks/render-block.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button } from '@ukhomeoffice/react-components';
 
 function Image(props) {
+  console.log('RENDERING IMAGE')
   return (
     <div className="image-wrapper">
       <div className="image-overlay">
@@ -15,7 +16,7 @@ function Image(props) {
 const renderBlock = (props, editor, next) => {
   const { attributes, children, node, isFocused } = props;
 
-  if (props.readOnly && !node.text.trim().length) {
+  if (node.type !== 'image' && props.readOnly && !node.text.trim().length) {
     return null;
   }
 
