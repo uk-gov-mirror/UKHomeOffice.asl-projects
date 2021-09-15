989f4b862f20a113ebe15cbd5821fc8eb44332d4
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2021-09-15 14:38:42 +0100

    add a link to the protocol summary table on application

--- client/pages/sections/protocols/editable-protocols.js
+++ client/pages/sections/protocols/editable-protocols.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import Protocols from './protocols';
 import Controls from '../../../components/controls';
 import ProtocolConditions from './protocol-conditions';
 
 const EditableProtocols = ({ advance, exit, ...props }) => (
   <Fragment>
+    <p><Link to="/protocol-summary" target="_blank">View summary table (opens in a new tab)</Link></p>
     <ProtocolConditions />
 
     <h2>Protocol details</h2>
