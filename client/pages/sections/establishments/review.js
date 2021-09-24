a1ba79f32e98255b71f3de23a255dea397bd8cec
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2021-09-24 13:03:48 +0100

    only show review banner in readonly view

--- client/pages/sections/establishments/review.js
+++ client/pages/sections/establishments/review.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Establishments from './establishments';
 import Banner from '../../../components/banner';
 import ReviewFields from '../../../components/review-fields';
 
 export default function EstablishmentsReview({ steps, ...props }) {
+  const { readonly } = useSelector(state => state.application);
+
   return (
     <div className="establishments-review">
-      <Banner>
-        <h2>Please review your answers for:</h2>
-        <h1>Establishments</h1>
-      </Banner>
+      {
+        !readonly &&
+          <Banner>
+            <h2>Please review your answers for:</h2>
+            <h1>Establishments</h1>
+          </Banner>
+      }
 
       <ReviewFields {...props} fields={steps[0].fields} />
 
@@ -18,7 +24,6 @@ export default function EstablishmentsReview({ steps, ...props }) {
       </div>
 
       <ReviewFields {...props} fields={steps[2].fields} />
-
     </div>
   )
 }
