44efe428829321650ed55324b6aaaa1cc90d50c7
Leonard Martin <leonard.martin@gmail.com>
2022-05-13 17:14:57 +0100

    Convert duration field to functional component

--- client/components/duration.js
+++ client/components/duration.js
@@ -1,71 +1,66 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import classnames from 'classnames';
 
 import Fieldset from './fieldset'
 
 const getNumbers = length => Array.apply(null, { length }).map(Number.call, String);
 
-class Duration extends Component {
-  state = this.props.value || {
-    years: null,
-    months: null
-  }
-
-  UNSAFE_componentWillReceiveProps({ value }) {
-    if (value) {
-      this.setState(value);
+const getFields = (years) => {
+  return [
+    {
+      name: 'years',
+      label: 'Years',
+      type: 'select',
+      options: getNumbers(6)
+    },
+    {
+      name: 'months',
+      label: 'Months',
+      type: 'select',
+      options: getNumbers(years < 5 ? 12 : 1)
     }
-  }
+  ]
+};
 
-  onChange = (key, value) => {
+const Duration = ({ name, error, label, hint, value, onChange }) => {
+
+  const [years, setYears] = useState(value.years);
+  const [months, setMonths] = useState(value.months);
+
+  useEffect(() => {
+    onChange({ months, years });
+  }, [years, months]);
+
+  useEffect(() => {
+    if (years === 5) {
+      setMonths(0);
+    }
+  }, [years]);
+
+  const onFieldChange = (key, value) => {
     value = parseInt(value);
-    this.setState({ [key]: value }, () => {
-      if (this.state.years === 5) {
-        this.setState({ months: 0 }, this.save);
-      }
-      else {
-        this.save()
-      }
-    });
-  }
+    if (key === 'years') {
+      setYears(value);
+    }
+    if (key === 'months') {
+      setMonths(value);
+    }
+  };
 
-  save = () => {
-    this.props.onChange(this.state);
-  }
+  return (
+    <div className={classnames('govuk-form-group', 'duration', { 'govuk-form-group--error': error })}>
+      <label className="govuk-label" htmlFor={name}>{label}</label>
+      { hint && <span id={`${name}-hint`} className="govuk-hint">{hint}</span> }
+      { error && <span id={`${name}-error`} className="govuk-error-message">{error}</span> }
+      <Fieldset
+        fields={getFields(years)}
+        onFieldChange={onFieldChange}
+        values={{ years, months }}
+        noComments={true}
+      />
+    </div>
+  )
 
-  getFields = () => {
-    return [
-      {
-        name: 'years',
-        label: 'Years',
-        type: 'select',
-        options: getNumbers(6)
-      },
-      {
-        name: 'months',
-        label: 'Months',
-        type: 'select',
-        options: getNumbers(this.state.years < 5 ? 12 : 1)
-      }
-    ]
-  }
-
-  render() {
-    const { error } = this.props;
-    return (
-      <div className={classnames('govuk-form-group', 'duration', { 'govuk-form-group--error': error })}>
-        <label className="govuk-label" htmlFor={this.props.name}>{this.props.label}</label>
-        { this.props.hint && <span id={`${this.props.name}-hint`} className="govuk-hint">{this.props.hint}</span> }
-        { this.props.error && <span id={`${this.props.name}-error`} className="govuk-error-message">{this.props.error}</span> }
-        <Fieldset
-          fields={this.getFields()}
-          onFieldChange={this.onChange}
-          values={this.state}
-          noComments={true}
-        />
-      </div>
-    )
-  }
-}
+};
 
 export default Duration;
