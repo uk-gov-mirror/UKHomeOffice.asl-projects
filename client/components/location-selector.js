ea6c4a9c583e96ee712dd0509791e942d6d573e4
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2022-05-16 17:46:50 +0100

    convert to functional component

--- client/components/location-selector.js
+++ client/components/location-selector.js
@@ -1,29 +1,27 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-
 import Field from './field';
 import getLocations from '../helpers/get-locations';
 
-const LocationSelector = ({
-  locations,
-  ...props
-}) => (
-  <div className="location-selector">
-    <Field
-      {...props}
-      type="checkbox"
-      className="smaller"
-      options={locations}
-      noComments={true}
-    />
-    <Link to="../establishments">Manage establishments</Link>
-    <Link to="../poles">Manage POLEs</Link>
-  </div>
-)
+const LocationSelector = (props) => {
+  const project = useSelector(state => state.project);
+  const { establishment } = useSelector(state => state.application);
+  const locations = getLocations(project, establishment);
 
-const mapStateToProps = ({ project, application: { establishment } }) => ({
-  locations: getLocations(project, establishment)
-});
+  return (
+    <div className="location-selector">
+      <Field
+        {...props}
+        type="checkbox"
+        className="smaller"
+        options={locations}
+        noComments={true}
+      />
+      <Link to="../establishments">Manage establishments</Link>
+      <Link to="../poles">Manage POLEs</Link>
+    </div>
+  );
+};
 
-export default connect(mapStateToProps)(LocationSelector);
+export default LocationSelector;
