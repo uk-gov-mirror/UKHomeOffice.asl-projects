ca655f7928becd18314f7351bbe49ace1ae8dcdc
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2022-05-16 17:46:24 +0100

    AA being removed should not be available as a proto location

--- client/helpers/get-locations.js
+++ client/helpers/get-locations.js
@@ -1,6 +1,11 @@
 export default function getLocations (project, establishment) {
-  const establishments = (project.establishments || []).filter(e => e.name || e['establishment-name']).map(e => e.name || e['establishment-name']);
+  const establishments = (project.establishments || [])
+    .filter(e => e.name || e['establishment-name'])
+    .filter(e => !e.deleted) // AA which is being removed during amendment
+    .map(e => e.name || e['establishment-name']);
+
   const poles = (project.polesList || []).filter(p => p.title).map(p => p.title);
+
   return [
     project.transferToEstablishmentName || establishment.name,
     ...establishments,
