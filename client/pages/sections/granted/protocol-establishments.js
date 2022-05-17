8e65a24a499c8a0b658e013f6b5e7fde04426a7b
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2022-05-17 10:31:44 +0100

    do not hide invalid protocol locations automatically

--- client/pages/sections/granted/protocol-establishments.js
+++ client/pages/sections/granted/protocol-establishments.js
@@ -3,15 +3,10 @@ import { useSelector } from 'react-redux';
 import get from 'lodash/get';
 import ReviewFields from '../../../components/review-fields';
 import { getSubsections } from '../../../schema';
-import getLocations from '../../../helpers/get-locations';
 
 const Locations = ({ values }) => {
-  const { project, establishment, schemaVersion } = useSelector(state => state.application);
+  const { schemaVersion } = useSelector(state => state.application);
   const fields = get(getSubsections(schemaVersion), 'protocols.sections.details.fields');
-
-  // older project transfers did not remove the previous primary establishment from the protocol locations
-  const projectLocations = getLocations(project, establishment);
-  values.locations = values.locations.filter(location => projectLocations.includes(location));
 
   return (
     <div className="locations">
