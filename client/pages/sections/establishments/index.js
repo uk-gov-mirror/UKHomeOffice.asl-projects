8fb553c3048bed924a9731b5e5a837a8bead52b1
Max O'Sullivan <Max.OSullivan@digital.homeoffice.gov.uk>
2022-06-24 15:28:03 +0100

    Edit establishments page to add more info when adding additionl establishments

--- client/pages/sections/establishments/index.js
+++ client/pages/sections/establishments/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Establishments from './establishments';
 import Controls from '../../../components/controls';
+import EstablishmentSelector from "../../../components/establishment-selector";
 
 export default function Index({ advance, exit, ...props }) {
   return (
     <div className="establishments-section">
       <h1>Establishments</h1>
+      <p className="larger">Add any additional establishments where work on this project will be carried out beyond just the primary establishment.</p>
+      <EstablishmentSelector {...props} review={false} larger={true}></EstablishmentSelector>
       <Establishments {...props} editable={true} />
       <Controls onContinue={advance} onExit={exit} />
     </div>
