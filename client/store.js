f597aa20f6b11865009c867cd4084aae4c95a210
kiran-varma-home-office <kiran.varma@digital.homeoffice.gov.uk>
2024-07-03 12:19:56 +0100

    Revert "feat: add redux dev tools"
    This reverts commit d7a8559cccd82dba8ea0eeba9244bf01ee95f17c.

--- client/store.js
+++ client/store.js
@@ -1,23 +1,13 @@
 import {createStore, applyMiddleware} from 'redux';
 import reducer from './reducers';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 const middlewares = [thunk];
 
 export default function configureStore(initialState = {}) {
-
-  let middleware;
-
-  if (process.env.NODE_ENV === 'development') {
-    middleware = composeWithDevTools(applyMiddleware(...middlewares));
-  } else {
-    middleware = applyMiddleware(...middlewares);
-  }
-
   return createStore(
     reducer,
     initialState,
-    middleware
+    applyMiddleware(...middlewares)
   );
 }
